2e4342bcad8c7f1d9406c0a5a1ff11a2c0aa0f95
Kyrr0 <Kyrr0@cuvox.de>
2015-09-17 15:00:36 +0200

    Create Offsets.h

---
+++ Offsets.h
@@ -0,0 +1,9 @@
+#ifndef MY_GLOBALS_H
+#define MY_GLOBALS_H
+const DWORD m_dwEntityList = 0x04A359D4;
+const DWORD m_dwLocalPlayerIndex = 0x00000160;
+const DWORD m_dwGlowObject = 0x04B48F04;
+const DWORD m_iGlowIndex = 0x000086B0;
+const DWORD m_iTeamNum = 0x000000F0;
+const DWORD m_bDormant = 0x000000E9;
+#endif
